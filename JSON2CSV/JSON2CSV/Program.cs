eaf6c9a3c1bf817d8f48f89d01adce7b5c6131f4
Ciwan Clifford <18048714+mlgwan@users.noreply.github.com>
2025-11-25 08:41:51 +0100

    Add project files.

---
+++ JSON2CSV/JSON2CSV/Program.cs
@@ -0,0 +1,42 @@
+using JSON2CSV.Client.Pages;
+using JSON2CSV.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UsePathBase("/json2csv");
+app.Use((context, next) =>
+{
+    if (context.Request.Path.StartsWithSegments("/json2csv", out var remainder))
+    {
+        context.Request.Path = remainder;
+    }
+    return next(context);
+});
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(JSON2CSV.Client._Imports).Assembly);
+
+app.Run();
