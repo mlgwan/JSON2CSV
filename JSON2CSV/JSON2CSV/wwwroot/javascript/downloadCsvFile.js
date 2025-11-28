b5638ffbab75e9055fc8463bdf65a28cf48f688d
Ciwan Clifford <18048714+mlgwan@users.noreply.github.com>
2025-11-28 11:56:58 +0100

    feat: Copy to Clipboard and Download buttons

---
+++ JSON2CSV/JSON2CSV/wwwroot/javascript/downloadCsvFile.js
@@ -0,0 +1,11 @@
+﻿window.downloadCsvFile = (fileName, content) => {
+    const blob = new Blob([content], { type: "text/csv" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
