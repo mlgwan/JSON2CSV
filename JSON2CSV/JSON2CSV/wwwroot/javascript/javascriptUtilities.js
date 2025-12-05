048566056fdb4d403d98d2a64756bd9f5cd121af
Ciwan Clifford <18048714+mlgwan@users.noreply.github.com>
2025-12-05 10:33:26 +0100

    refactor: Consolidate JS-Interop functions into one file

---
+++ JSON2CSV/JSON2CSV/wwwroot/javascript/javascriptUtilities.js
@@ -0,0 +1,34 @@
+﻿window.clipboardCopy = {
+    copyText: function (text) {
+        navigator.clipboard.writeText(text).then(function () {
+            alert("Copied to clipboard!");
+        })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+};
+
+window.downloadCsvFile = (fileName, content) => {
+    const blob = new Blob([content], { type: "text/csv" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
+
+window.downloadJsonFile = (fileName, content) => {
+    const blob = new Blob([content], { type: "text/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
